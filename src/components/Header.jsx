84ba84ef079ed0d4cecd8568ff53adf7b975b3c1
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-07-24 18:06:45 +0300

    template created

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://cdn2.iconfinder.com/data/icons/playstation-controller-buttons-3/64/playstation-flat-icon-triangle-dark-128.png" />
+    </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://wallpapersmug.com/download/3840x2160/781f69/fantasy-flying-city-dark-art.jpg' />
+        </div>
+        <div>
+            <img src='https://previews.123rf.com/images/bigio/bigio1602/bigio160200004/52547413-sch%C3%B6ne-zuckersch%C3%A4del-frau-illustration-tag-der-toten-vektor-illustration-.jpg' />
+        </div>
+    </div>;
+}
+
+export default Profile;
