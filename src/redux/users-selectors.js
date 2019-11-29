e31ade767bb106f185be691f21d8cb847c974dc5
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2019-11-29 15:50:56 +0300

    fixed json

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -4,9 +4,11 @@ import { createSelector } from 'reselect';
 export const getUsersSelector = (state) => {
     return state.usersPage.users;
 }
+
 export const getUsers = createSelector( getUsersSelector, (users) => {
     return users.filter(u => true);
 })
+
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
 }
